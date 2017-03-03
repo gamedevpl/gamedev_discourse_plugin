4a49dbf88dc8673d231d48ea97843e5bf3d72fe2
Grzegorz Tanczyk <fb@tanczyk.pl>
2017-03-03 21:12:59 +0100

    multiple links

--- plugin.rb
+++ plugin.rb
@@ -3,4 +3,7 @@
 # version: 0.0.2
 # authors: Grzegorz Tańczyk
 
-register_custom_html(extraNavItem: %(<li class="ember-view"><a href="http://www.gamedev.pl" title="Serwis o programowaniu gier - Gamedev.pl">Gamedev.pl</a></li>))
+register_custom_html(extraNavItem: %(
+  <li class="ember-view"><a href="http://www.gamedev.pl" title="Serwis o programowaniu gier - Gamedev.pl">Gamedev.pl</a></li>
+  <li class="ember-view"><a href="http://polskastrefa.pl" title="Ogłoszenia - PolskaStrefa.pl">Polska Strefa</a></li>
+))
