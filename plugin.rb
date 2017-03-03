5e9ef15eb031b21e7105ccfb36901ca4a9d0de72
Grzegorz Tanczyk <fb@tanczyk.pl>
2017-03-03 20:47:56 +0100

    use register_custom_html instead

--- plugin.rb
+++ plugin.rb
@@ -2,3 +2,5 @@
 # about: Customizations for https://forum.warsztat.io
 # version: 0.0.2
 # authors: Grzegorz Tańczyk
+
+register_custom_html(extraNavItem: %(<li class="ember-view"><a href="http://www.gamedev.pl" title="Serwis o programowaniu gier - Gamedev.pl">Gamedev.pl</a></li>))
